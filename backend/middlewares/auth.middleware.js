fa81074b1e0d4fe57ad066108269d36de0f3d748
Ahmad Rammal <ahmad.rammal2023@gmail.com>
2025-01-23 22:25:28 +0200

    Backend <> Create JWT Middleware

---
+++ backend/middlewares/auth.middleware.js
@@ -0,0 +1,22 @@
+const jwt = require("jsonwebtoken");
+const User = require("../models/user.model");
+
+const authMiddleware = async (req, res, next) => {
+  try {
+    const token = req.headers["authorization"]?.split(" ")[1];
+    if (!token) {
+      return res.status(403).send("Forbidden");
+    } else {
+      const decoded = jwt.verify(token, process.env.JWT_SECRET);
+      const user = await User.findOne({ email: decoded.email }).select(
+        "-password"
+      );
+      req.user = user;
+      next();
+    }
+  } catch (error) {
+    return res.status(500).send("Expired JWT");
+  }
+};
+
+module.exports = authMiddleware;
