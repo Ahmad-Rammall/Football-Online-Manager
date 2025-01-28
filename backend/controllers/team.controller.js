4965766bcf25a91953aba07dd0679f7f8139d355
Ahmad Rammal <ahmad.rammal2023@gmail.com>
2025-01-28 21:45:50 +0200

    Backend <> Create Update Team Name API

--- backend/controllers/team.controller.js
+++ backend/controllers/team.controller.js
@@ -12,7 +12,9 @@ const getTeam = async (req, res) => {
 
   const players = await Player.find({
     teamId: team._id,
-  }).populate("position").lean();
+  })
+    .populate("position")
+    .lean();
 
   return res.status(200).json({
     players,
@@ -20,6 +22,22 @@ const getTeam = async (req, res) => {
   });
 };
 
+const updateTeamName = async (req, res) => {
+  const userId = req.user._id;
+  const teamName = req.body.teamName;
+
+  const team = await Team.findOne({
+    userId,
+  });
+
+  if (!team) return res.status(400).json({ message: "User Has No Team!" });
+
+  team.teamName = teamName;
+  await team.save();
+  return res.status(200).json({ message: "Team Name Updated" });
+};
+
 module.exports = {
   getTeam,
+  updateTeamName,
 };
--- backend/routes/team.routes.js
+++ backend/routes/team.routes.js
@@ -1,8 +1,9 @@
 const express = require("express");
-const { getTeam } = require("../controllers/team.controller");
+const { getTeam, updateTeamName } = require("../controllers/team.controller");
 const authMiddleware = require("../middlewares/auth.middleware");
 const router = express.Router();
 
 router.get("/", authMiddleware, getTeam);
+router.put("/", authMiddleware, updateTeamName)
 
 module.exports = router;
